5aef287b073620ee56de2fad8d7cc6676edc3d25
Sebastian Schulze <github.com@bascht.com>
2014-05-02 18:00:10 +0200

    Kill gauges for Piwik. \o/

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,13 +1,1 @@
 $(document).foundation();
-
-var _gauges = _gauges || [];
-(function() {
-  var t   = document.createElement('script');
-  t.type  = 'text/javascript';
-  t.async = true;
-  t.id    = 'gauges-tracker';
-  t.setAttribute('data-site-id', '50b46e41613f5d03a7000001');
-  t.src = '//secure.gaug.es/track.js';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(t, s);
-})();
